5a318d49d9b2b75f0099ce353d652361c3a44ea7
William Bonitz <wbnhw3@gmail.com>
2016-07-21 18:27:06 -0700

    mn s.g, v
    policeman!

--- src/Houses.java
+++ src/Houses.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+
+import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 
 public class Houses {
@@ -9,22 +12,45 @@ public static void main(String[]args){
 	for(int i = 0;i<10;i++)
 
 	{
-		drawHouse();
+		drawHouse("small","orange");
+		drawHouse("medium","orange");
+		drawHouse("large","orange");
 
 	}
 }
 
 
 
-static void drawHouse(){
-	Tortoise.move(100);
-	for (int i = 0; i < 2; i++) {
-		Tortoise.turn(60);
+static void drawHouse( String size,String color){
+	int height=0;
+	if(size.equals("small")){
+		height=10;
+	}
+	if(size.equals("medium")){
+		height=100;
+	}
+	if(size.equals("large")){
+		height=200;
+	}
+	if(color.equals("blue")){
+		Tortoise.setPenColor(Color.BLUE);
+	}
+	if(color.equals("gray")){
+		Tortoise.setPenColor(Color.GRAY);
+	}
+	if(color.equals("orange")){
+		Tortoise.setPenColor(Color.orange);;
+	}
+	Tortoise.move(height);
+	for (int i = 0; i < 1; i++) {
+		Tortoise.turn(90);
 		Tortoise.move(40);
 		}
-	Tortoise.turn(60);
-	Tortoise.move(100);
-	Tortoise.turn(-90);
+	Tortoise.turn(90);
+	Tortoise.move(height);
+	Tortoise.turn(270);
+	Tortoise.setPenColor(Color.green);
 	Tortoise.move(30);
+	Tortoise.turn(270);
 }
 }
