b1a895928ba3f2c9ac60b4ae1624477615c863c1
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-01-05 18:26:10 -0800

    fast

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -38,26 +38,35 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 int mouseX = e.getX();
    	 int mouseY = e.getY();
    	 // 5. Print the mouseX variable
-   	 System.out.println(mouseX);
+   	 String xString= Integer.toString(mouseX);
+   	 String yString= Integer.toString(mouseY);
+   	 System.out.println(xString+","+yString);
    	 // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
    	 // 7. Adjust your secret location co-ordinates here:
-   	 int secretLocationX = 0;
-   	 int secretLocationY = 0;
+   	 int secretLocationX = 237;
+   	 int secretLocationY = 339;
    	 /** If the mouse co-ordinates and secret location are close, we'll let them ask a question. */
-   	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
+   	 
+   	 
+//   	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
+   	 
+   		 
+   		 
    		 // 8. Get the user to enter a question for the fortune teller
-
-   		 // 9. Find a spooky sound and put it in your default package (freesound.org)
-   		 // AudioClip sound = JApplet.newAudioClip(getClass().getResource("spooky.aif"));
-   		 // 10. Play the sound
-
-   		 // 11. Use the pause() method below to wait until your music has finished
-
-   		 // 12. Insert your completed Magic 8 ball recipe (http://bit.ly/Zdrf6d) here
-
-   	 }
-
+   	String muse= JOptionPane.showInputDialog("Enter a question.");
+   	 // 9. Find a spooky sound and put it in your default package (freesound.org)
+     AudioClip sound = JApplet.newAudioClip(getClass().getResource("magic.wav"));
+   	 // 10. Play the sound
+    sound.play();
+   	 // 11. Use the pause() method below to wait until your music has finished
+     pause(2000);
     }
+   		 // 12. Insert your completed Magic 8 ball recipe (http://bit.ly/Zdrf6d) here
+   		 
+   	 
+
+    
+    
 
     private boolean areClose(int mouseX, int secretLocationX) {
    	 return mouseX < secretLocationX + 15 && mouseX > secretLocationX - 15;
@@ -103,18 +112,17 @@ private void showAnotherImage(String imageName) {
     public void paintComponent(Graphics g) {
    	 g.drawImage(fortuneTellerImage, 0, 0, null);
     }
-
     @Override
-    public void mouseClicked(MouseEvent e) {}
-
+    public void mouseClicked(MouseEvent e) {
+    }
     @Override
-    public void mouseReleased(MouseEvent e) {}
-
+    public void mouseReleased(MouseEvent e) {   	
+    }
     @Override
-    public void mouseEntered(MouseEvent e) {}
-
+    public void mouseEntered(MouseEvent e) {
+    }
     @Override
-    public void mouseExited(MouseEvent e) {}
-
+    public void mouseExited(MouseEvent e) {
+    }
 }
 
