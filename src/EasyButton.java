aff973298d35f83a828781b61a92875e6ba27189
najclark <league@WTS7.attlocal.net>
2017-01-26 18:29:27 -0800

    g fk;lk';k;

---
+++ src/EasyButton.java
@@ -0,0 +1,102 @@
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class EasyButton implements MouseListener,KeyListener{
+
+	/*
+	 * Use the methods below to make an easy button show on the screen. 
+	 * When clicked, it says some very annoying words.
+	 */
+public static void main(String[]args){
+EasyButton easy= new EasyButton();
+easy.showEasyButton();
+}
+JLabel easyButtonImage;
+	private void showEasyButton() {
+		JFrame quizWindow = new JFrame();
+	
+		quizWindow.setVisible(true);
+		URL imageURL = null;
+		try {
+			imageURL = new File("/Users/league/Google Drive/league-images/easy_button.jpg").toURI().toURL();
+		} catch (MalformedURLException e) {
+			System.err.println("Could not load file: " + imageURL);
+		}
+		Icon icon = new ImageIcon(imageURL);
+		this.easyButtonImage = new JLabel(icon);
+		quizWindow.add(easyButtonImage);
+		quizWindow.pack();	
+		quizWindow.addMouseListener(this);
+		quizWindow.addKeyListener(this);
+	}
+
+
+	private void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	speak("Spacebar");
+	
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode()==(KeyEvent.VK_SPACE)){
+		System.exit(0);
+		}
+		System.out.println(e.getKeyCode());
+		System.out.println(KeyEvent.VK_SPACE);
+	
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
