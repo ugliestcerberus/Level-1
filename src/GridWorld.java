ebcda4942d01a9df80b3c05265ceccbafa1a3cb6
Love4653 <league@iMac-2.attlocal.net>
2017-04-13 18:27:28 -0700

    asdfasdf

---
+++ src/GridWorld.java
@@ -0,0 +1,34 @@
+import info.gridworld.actor.Bug;
+import info.gridworld.world.World;
+
+// Teachers: Need Gridworld handouts (4 handouts with descriptions of World, Bug, Grid and Location objects. look for laminated sheets.)
+
+//In this exercise we’ll use OBJECTS and METHODS to make a world that contains bugs and flowers.
+
+//Before you start, you need to add the gridworld.jar to your Level 1 project. 
+
+//Right click your project, then click Properties > Java Build Path > Libraries > Add External JAR.
+
+//Select the gridworld.jar under the Google Drive > league-jars, then say OK.
+
+
+
+//Use the laminated handout to complete these challenges:
+
+//Figure out how to get the World to show.
+//Figure out how to add a Bug to the world (clue: you will need the Bug and Location objects)
+//Add another bug at a random location in the world.
+//Change the color of that bug to blue.
+//Make the bug face to the right.
+//Add flowers to the left and right of the bug.
+//Fill the whole world with flowers!
+
+//[Optional] Color the flowers in rows like the American flag (red & white)
+//[Optional] Use a new class to make an X out of bugs.
+public class GridWorld{
+public static void main(String[] args){
+World namd= new World();
+namd.show();
+Bug has= new Bug();
+}
+}
--- src/MrsMinion.java
+++ src/MrsMinion.java
@@ -1,4 +1,45 @@
-
 public class MrsMinion {
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
 
+public MrsMinion(String name,int eyes,String color,String master) {
+		 this.eyes= eyes;	
+		 this.name= name;
+		 this.color=color;
+		 this.master=master;
+	}
+
+	void setName(String newName) {
+		name = newName;
+	}
+
+	String getName() {
+		return name;
+	}
+
+	void setEyes(int newEyes) {
+		eyes = newEyes;
+	}
+
+	int getEyes() {
+		return eyes;
+	}
+
+	void setColor(String newColor) {
+		color = newColor;
+	}
+
+	String getColor(){
+		return color;
+	}
+	
+	void setMaster(String newMaster){
+		master= newMaster;
+	}
+	
+	String getMaster(){
+		return master;
+	}
 }
