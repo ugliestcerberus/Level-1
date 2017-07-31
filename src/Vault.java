1e74094edfba5b2f41e14b4f65715f116c5fd534
Ssuratkal2021 <league@WorthyMini-5.attlocal.net>
2017-07-31 11:57:10 -0700

    jack. jah

--- src/Vault.java
+++ src/Vault.java
@@ -1,21 +1,49 @@
 import java.util.Random;
+
 public class Vault {
-int code;
-Vault(){	
-code=new Random().nextInt(1000000)+1;
-}
-}
-class Spy{
-	String name;
-	Spy(String name){
-	this.name= name; 
-	Spy name1= new Spy("JamesBond");
+	public static void main(String[] args) {
+		Vault v = new Vault();
+		JamesBond d= new JamesBond();
+		System.out.println(v.tryCode(code, guess));
+	}
+
+	static int guess;
+	static int code;
+
+	Vault() {
+
+		code = new Random().nextInt(1000000) + 1;
+	}
+
+	boolean tryCode(int code, int guess) {
+		return (guess == code);
 	}
 }
-class vaultCracker{
+
+class Spy {
+	String name;
+
+	Spy(String name) {
+		this.name = name;
+		Spy name10 = new Spy("JamesBond");
+	}
+}
+
+class vaultCracker {
 	Vault vault;
 	Spy spy;
-boolean tryCode(int code){
-return(guess==code);	
+
 }
+
+class JamesBond {
+
+	int findCode(Vault v) {
+		for(int i=0; i<1000000; i++){
+			v.guess= i;
+			if(v.tryCode(v.code,v.guess)){
+				return v.code;
+			}
+		}
+		return -1;
+	}
 }
