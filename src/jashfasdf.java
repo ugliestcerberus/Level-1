88f64e0a679b9f0f6a482258fbf8d496f1931b76
JackChine27 <league@WTS14.attlocal.net>
2017-09-07 18:27:03 -0700

    hagsgdsg

--- src/jashfasdf.java
+++ src/jashfasdf.java
@@ -1,19 +1,53 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-public class jashfasdf {
+public class jashfasdf implements ActionListener{
 static JFrame hasg= new JFrame();
-JPanel jash= new JPanel();
+static JPanel jash= new JPanel();
 static JButton jasdjg= new JButton();
-JButton jasdfsdf= new JButton();
-JButton jasf= new JButton();
+static JButton jasdfsdf= new JButton();
+static JButton jasf= new JButton();
+int sum;
+int num1;
+int num2;
 public static void main(String[] args) {
-	jashfasdf has= new jashfasdf();
+jashfasdf has= new jashfasdf();
 }
 jashfasdf(){
 hasg.setVisible(true);
-hasg.setSize(500,1000);
+hasg.setSize(1000,1000);
 hasg.add(jash);
+jash.add(jasdjg);
+jash.add(jasdfsdf);
+jash.add(jasf);
+jasdjg.setText("Random");
+jasdfsdf.setText("Greeting");
+jasf.setText("Math");
+jasf.addActionListener(this);
+jasdfsdf.addActionListener(this);
+jasdjg.addActionListener(this);
 }
-
+public void actionPerformed(ActionEvent e) {
+if(e.getSource()==jasdjg) {
+int number= new Random().nextInt(1000);
+JOptionPane.showMessageDialog(null, "Here's a random number " + number);
+}
+else if(e.getSource()==jasdfsdf) {
+JOptionPane.showMessageDialog(null, "Please enter a name");
+String name= JOptionPane.showInputDialog(null, "");
+JOptionPane.showMessageDialog(null, "Hello " + name);
+}
+else if(e.getSource()==jasf) {
+String jah= JOptionPane.showInputDialog(null,"Please enter a number");
+int x= Integer.parseInt(jah);
+String number= JOptionPane.showInputDialog(null,"Please enter a number");
+int as= Integer.parseInt(number);
+sum=x+as;
+JOptionPane.showMessageDialog(null,  x + "+" + as +"="+sum);
+}
+}
 }
